99ba75ca187dd71642c3e8284375a33f20754d13
rahmielibol <64936670+rahmielibol@users.noreply.github.com>
2021-10-19 18:33:06 +0300

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,23 @@
+from brian2 import * #Kütüphanelerin tanımlanması
+from numpy import * #Numeric python kütüphanesi temel matematiksel işlemlerin fonksiyonları
+from matplotlib.pyplot import * #Grafik çizdirme için gerekli kütüphane
+
+#Topla ve ateşle modelinin tanımlanması
+tau = 5*ms #tau değişkenine değer atama(Birimlere dikkat!)
+a=-1
+d=1
+# Çok satırlı ifadeleri yazmak için ''' kullanılır. Hücre denklemi bu şekilde tanımlanır.
+eqs = ''' 
+dv/dt = (a*v+d)/tau : 1
+'''
+G = NeuronGroup(1, eqs, threshold='v>0.8', reset='v = 0', method='linear')
+#G.v=0.1     #Baslangic kosulu
+# Modeli gerçekleyen temel fonksiyon hücre sayısı, denklemi, çözüm metodu gibi değişkenlere sahiptir.
+M = StateMonitor(G, 'v', record=0)
+# Model çalıştırıldığında tüm veriler tutulmaz. StateMonitor gibi fonksiyonlarla kaydedilmek istenen değişkenler seçilir.
+run(30*ms)
+# Model run komut ile belli bir süre işletilir. (Burada programın akışına dikkat!)
+
+plot(M.t/ms, M.v[0])# Kaydedilen veri grafiğe çizdirilir.
+xlabel('Time (ms)')
+ylabel('v')
